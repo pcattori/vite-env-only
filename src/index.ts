ed32c912ba43ea2991f3d16e9d862a690e6f1e29
Pedro Cattori <pcattori@gmail.com>
2024-01-04 14:38:28 -0500

    macros

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export const foo = "bar";
+const maybe = <T>(x: T): T | undefined => x
+
+/**
+ * On the server, returns the value passed in.
+ * On the client, returns `undefined`.
+ */
+export const server$ = maybe
+
+/**
+ * On the client, returns the value passed in.
+ * On the server, returns `undefined`.
+ */
+export const client$ = maybe
