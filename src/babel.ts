089c16f348b1298929cd93d9d1d4889fccbfe276
Pedro Cattori <pcattori@gmail.com>
2024-01-05 21:18:52 -0500

    refactor: babel re-exports

--- src/babel.ts
+++ src/babel.ts
@@ -1,18 +1,18 @@
-import type { NodePath } from "@babel/traverse"
-import type { types as BabelTypes } from "@babel/core"
-import { parse, type ParseResult } from "@babel/parser"
-import * as t from "@babel/types"
+export type { types as BabelTypes } from "@babel/core"
+export { parse, type ParseResult } from "@babel/parser"
+export type { NodePath } from "@babel/traverse"
+export * as t from "@babel/types"
 
 // Avoid CJS-ESM default export interop differences across different tools
 // https://github.com/babel/babel/issues/13855#issuecomment-945123514
+
 import { createRequire } from "node:module"
 const require = createRequire(import.meta.url)
+
 // @ts-expect-error
 import _traverse = require("@babel/traverse")
+export const traverse = _traverse.default
+
 // @ts-expect-error
 import _generate = require("@babel/generator")
-const traverse = _traverse.default
-const generate = _generate.default
-
-export { traverse, generate, parse, t }
-export type { BabelTypes, NodePath, ParseResult }
+export const generate = _generate.default
