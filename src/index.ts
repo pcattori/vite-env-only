6b8a1d60ac35eef5674cd8e182c380612ba82f4f
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-05-02 17:08:13 +1000

    fix: support `transformRequest`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { type PluginOption } from "vite"
+import { ResolvedConfig, type PluginOption } from "vite"
 import path from "node:path"
 
 import { name as pkgName } from "../package.json"
@@ -19,12 +19,14 @@ export default ({
   files: fileValidators,
 }: Options = {}): PluginOption[] => {
   let root: string
+  let command: ResolvedConfig["command"]
 
   return [
     {
       name: "vite-plugin-env-only",
       configResolved(config) {
         root = config.root
+        command = config.command
       },
       async transform(code, id, options) {
         if (!code.includes(pkgName)) return
@@ -51,6 +53,13 @@ export default ({
           name: "vite-plugin-env-only-files",
           enforce: "pre",
           async resolveId(id, importer, options) {
+            if (command !== "build" && importer?.endsWith(".html")) {
+              // Vite has a special `index.html` importer for `resolveId` within `transformRequest`
+              // https://github.com/vitejs/vite/blob/5684fcd8d27110d098b3e1c19d851f44251588f1/packages/vite/src/node/server/transformRequest.ts#L158
+              // https://github.com/vitejs/vite/blob/5684fcd8d27110d098b3e1c19d851f44251588f1/packages/vite/src/node/server/pluginContainer.ts#L668
+              return
+            }
+
             let isResolving =
               options?.custom?.["vite-plugin-env-only-files"] ?? false
 
