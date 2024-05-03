59027a7230a3ae446f976c9c71f9acfac3c7feb1
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-05-03 21:13:03 +1000

    test: refactor

--- src/validate-file.test.ts
+++ src/validate-file.test.ts
@@ -6,192 +6,194 @@ function fromCwd(relativePath: string) {
   return path.join(process.cwd(), relativePath)
 }
 
-test("validateFile / denyFiles.client / env:client / failed string validation", () => {
-  expect(() =>
-    validateFile({
-      absolutePath: fromCwd("/lib/server-only.ts"),
-      denyFiles: {
-        client: ["lib/server-only.ts"],
-        server: [],
-      },
-      root: fromCwd("/"),
-      importer: fromCwd("/path/to/importer.ts"),
-      env: "client",
-    })
-  ).toThrowErrorMatchingInlineSnapshot(
-    `
+describe("validateFile", () => {
+  test("denyFiles.client / env:client / failed string validation", () => {
+    expect(() =>
+      validateFile({
+        absolutePath: fromCwd("/lib/server-only.ts"),
+        denyFiles: {
+          client: ["lib/server-only.ts"],
+          server: [],
+        },
+        root: fromCwd("/"),
+        importer: fromCwd("/path/to/importer.ts"),
+        env: "client",
+      })
+    ).toThrowErrorMatchingInlineSnapshot(
+      `
     [Error: File denied in client environment
      - File: lib/server-only.ts
      - Importer: path/to/importer.ts
      - Matcher: "lib/server-only.ts"]
   `
-  )
-})
+    )
+  })
 
-test("validateFile / denyFiles.client / env:client / failed regex validation", () => {
-  expect(() =>
-    validateFile({
-      absolutePath: fromCwd("/lib/server-only.ts"),
-      denyFiles: {
-        client: [/^lib\/server-only\.ts$/],
-        server: [],
-      },
-      root: fromCwd("/"),
-      importer: fromCwd("/path/to/importer.ts"),
-      env: "client",
-    })
-  ).toThrowErrorMatchingInlineSnapshot(
-    `
+  test("denyFiles.client / env:client / failed regex validation", () => {
+    expect(() =>
+      validateFile({
+        absolutePath: fromCwd("/lib/server-only.ts"),
+        denyFiles: {
+          client: [/^lib\/server-only\.ts$/],
+          server: [],
+        },
+        root: fromCwd("/"),
+        importer: fromCwd("/path/to/importer.ts"),
+        env: "client",
+      })
+    ).toThrowErrorMatchingInlineSnapshot(
+      `
     [Error: File denied in client environment
      - File: lib/server-only.ts
      - Importer: path/to/importer.ts
      - Matcher: /^lib\\/server-only\\.ts$/]
   `
-  )
-})
+    )
+  })
 
-test("validateFile / denyFiles.client / env:client / failed validation without importer", () => {
-  expect(() =>
-    validateFile({
-      absolutePath: fromCwd("/lib/server-only.ts"),
-      denyFiles: {
-        client: ["lib/server-only.ts"],
-        server: [],
-      },
-      root: fromCwd("/"),
-      importer: undefined,
-      env: "client",
-    })
-  ).toThrowErrorMatchingInlineSnapshot(
-    `
+  test("denyFiles.client / env:client / failed validation without importer", () => {
+    expect(() =>
+      validateFile({
+        absolutePath: fromCwd("/lib/server-only.ts"),
+        denyFiles: {
+          client: ["lib/server-only.ts"],
+          server: [],
+        },
+        root: fromCwd("/"),
+        importer: undefined,
+        env: "client",
+      })
+    ).toThrowErrorMatchingInlineSnapshot(
+      `
     [Error: File denied in client environment
      - File: lib/server-only.ts
      - Matcher: "lib/server-only.ts"]
   `
-  )
-})
+    )
+  })
 
-test("validateFile / denyFiles.client / env:client / passed validation", () => {
-  expect(() =>
-    validateFile({
-      absolutePath: fromCwd("/lib/some-other-file.ts"),
-      denyFiles: {
-        client: [/server-only/],
-        server: [],
-      },
-      root: fromCwd("/"),
-      importer: fromCwd("/path/to/importer.ts"),
-      env: "client",
-    })
-  ).not.toThrow()
-})
+  test("denyFiles.client / env:client / passed validation", () => {
+    expect(() =>
+      validateFile({
+        absolutePath: fromCwd("/lib/some-other-file.ts"),
+        denyFiles: {
+          client: [/server-only/],
+          server: [],
+        },
+        root: fromCwd("/"),
+        importer: fromCwd("/path/to/importer.ts"),
+        env: "client",
+      })
+    ).not.toThrow()
+  })
 
-test("validateFile / denyFiles.client / env:server / ignores denyFiles.client", () => {
-  expect(() =>
-    validateFile({
-      absolutePath: fromCwd("/lib/server-only.ts"),
-      denyFiles: {
-        client: ["lib/server-only.ts"],
-        server: [],
-      },
-      root: fromCwd("/"),
-      importer: fromCwd("/path/to/importer.ts"),
-      env: "server",
-    })
-  ).not.toThrow()
-})
+  test("denyFiles.server / env:client / ignores denyFiles.server", () => {
+    expect(() =>
+      validateFile({
+        absolutePath: fromCwd("/lib/client-only.ts"),
+        denyFiles: {
+          client: [],
+          server: ["/lib/client-only.ts"],
+        },
+        root: fromCwd("/"),
+        importer: fromCwd("/path/to/importer.ts"),
+        env: "client",
+      })
+    ).not.toThrow()
+  })
 
-test("validateFile / denyFiles.server / env:server / failed string validation", () => {
-  expect(() =>
-    validateFile({
-      absolutePath: fromCwd("/lib/client-only.js"),
-      denyFiles: {
-        client: [],
-        server: ["lib/client-only.js"],
-      },
-      root: fromCwd("/"),
-      importer: fromCwd("/path/to/importer.ts"),
-      env: "server",
-    })
-  ).toThrowErrorMatchingInlineSnapshot(
-    `
+  test("denyFiles.client / env:server / ignores denyFiles.client", () => {
+    expect(() =>
+      validateFile({
+        absolutePath: fromCwd("/lib/server-only.ts"),
+        denyFiles: {
+          client: ["lib/server-only.ts"],
+          server: [],
+        },
+        root: fromCwd("/"),
+        importer: fromCwd("/path/to/importer.ts"),
+        env: "server",
+      })
+    ).not.toThrow()
+  })
+
+  test("denyFiles.server / env:server / failed string validation", () => {
+    expect(() =>
+      validateFile({
+        absolutePath: fromCwd("/lib/client-only.js"),
+        denyFiles: {
+          client: [],
+          server: ["lib/client-only.js"],
+        },
+        root: fromCwd("/"),
+        importer: fromCwd("/path/to/importer.ts"),
+        env: "server",
+      })
+    ).toThrowErrorMatchingInlineSnapshot(
+      `
     [Error: File denied in server environment
      - File: lib/client-only.js
      - Importer: path/to/importer.ts
      - Matcher: "lib/client-only.js"]
   `
-  )
-})
+    )
+  })
 
-test("validateFile / denyFiles.server / env:server / failed regex validation", () => {
-  expect(() =>
-    validateFile({
-      absolutePath: fromCwd("/lib/client-only.ts"),
-      denyFiles: {
-        client: [],
-        server: [/^lib\/client-only\.ts$/],
-      },
-      root: fromCwd("/"),
-      importer: fromCwd("/path/to/importer.ts"),
-      env: "server",
-    })
-  ).toThrowErrorMatchingInlineSnapshot(
-    `
+  test("denyFiles.server / env:server / failed regex validation", () => {
+    expect(() =>
+      validateFile({
+        absolutePath: fromCwd("/lib/client-only.ts"),
+        denyFiles: {
+          client: [],
+          server: [/^lib\/client-only\.ts$/],
+        },
+        root: fromCwd("/"),
+        importer: fromCwd("/path/to/importer.ts"),
+        env: "server",
+      })
+    ).toThrowErrorMatchingInlineSnapshot(
+      `
     [Error: File denied in server environment
      - File: lib/client-only.ts
      - Importer: path/to/importer.ts
      - Matcher: /^lib\\/client-only\\.ts$/]
   `
-  )
-})
+    )
+  })
 
-test("validateFile / denyFiles.server / env:server / failed validation without importer", () => {
-  expect(() =>
-    validateFile({
-      absolutePath: fromCwd("/lib/client-only.ts"),
-      denyFiles: {
-        client: [],
-        server: ["lib/client-only.ts"],
-      },
-      root: fromCwd("/"),
-      importer: undefined,
-      env: "server",
-    })
-  ).toThrowErrorMatchingInlineSnapshot(
-    `
+  test("denyFiles.server / env:server / failed validation without importer", () => {
+    expect(() =>
+      validateFile({
+        absolutePath: fromCwd("/lib/client-only.ts"),
+        denyFiles: {
+          client: [],
+          server: ["lib/client-only.ts"],
+        },
+        root: fromCwd("/"),
+        importer: undefined,
+        env: "server",
+      })
+    ).toThrowErrorMatchingInlineSnapshot(
+      `
     [Error: File denied in server environment
      - File: lib/client-only.ts
      - Matcher: "lib/client-only.ts"]
   `
-  )
-})
+    )
+  })
 
-test("validateFile / denyFiles.server / env:server / passed validation", () => {
-  expect(() =>
-    validateFile({
-      absolutePath: fromCwd("/lib/some-other-file.ts"),
-      denyFiles: {
-        client: [],
-        server: ["lib/client-only.ts"],
-      },
-      root: fromCwd("/"),
-      importer: fromCwd("/path/to/importer.ts"),
-      env: "server",
-    })
-  ).not.toThrow()
-})
-
-test("validateFile / denyFiles.server / env:client / ignores denyFiles.server", () => {
-  expect(() =>
-    validateFile({
-      absolutePath: fromCwd("/lib/client-only.ts"),
-      denyFiles: {
-        client: [],
-        server: ["/lib/client-only.ts"],
-      },
-      root: fromCwd("/"),
-      importer: fromCwd("/path/to/importer.ts"),
-      env: "client",
-    })
-  ).not.toThrow()
+  test("denyFiles.server / env:server / passed validation", () => {
+    expect(() =>
+      validateFile({
+        absolutePath: fromCwd("/lib/some-other-file.ts"),
+        denyFiles: {
+          client: [],
+          server: ["lib/client-only.ts"],
+        },
+        root: fromCwd("/"),
+        importer: fromCwd("/path/to/importer.ts"),
+        env: "server",
+      })
+    ).not.toThrow()
+  })
 })
